e35642d679f64c3c692e1ebbea07f53edb94db0c
Cameron Campbell <cameroncampbell@Camerons-MacBook-Pro.local>
2024-07-20 17:33:42 +0100

    fixed weird bug with buildDocs script

--- docs/buildDocs.ts
+++ docs/buildDocs.ts
@@ -160,6 +160,7 @@ const buildDocsForApis = async (apis: Directory[], apisName: "classic" | "cloud"
     //if (apiName !== "standardDataStores_V1") continue
   
     const methods = apiFile?.getExportSymbols()
+    if (apiName == "standardDataStores_V2") console.log(methods)
     if (!methods) continue
 
     for (const method of methods) {
