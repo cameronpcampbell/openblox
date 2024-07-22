79b39cc4a22086c7a51167b1849d12d1272d5761
Cameron Campbell <cameroncampbell@Camerons-MacBook-Pro.local>
2024-07-22 22:52:19 +0100

    .

--- docs/buildDocs.ts
+++ docs/buildDocs.ts
@@ -160,7 +160,7 @@ const buildDocsForApis = async (apis: Directory[], apisName: "classic" | "cloud"
     //if (apiName !== "standardDataStores_V1") continue
   
     const methods = apiFile?.getExportSymbols()
-    if (apiName == "standardDataStores_V2") console.log(`${api.getPath()}/${apiName}.ts`)
+    if (apiName == "standardDataStores_V2") console.log(methods)
     if (!methods) continue
 
     for (const method of methods) {
