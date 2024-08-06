77707b9761908dcfc0bcf937b3f9b209ecacbdd4
Cameron Campbell <cameroncampbell@Camerons-MacBook-Pro.local>
2024-08-06 13:41:10 +0100

    fixed jsdoc params for experience apis update place and universe

--- src/apis/cloud/experiences/experiences.ts
+++ src/apis/cloud/experiences/experiences.ts
@@ -172,7 +172,7 @@ export const placeInfo = addApiMethod(async <UniverseId extends Identifier, Plac
  * 
  * @example
  * const { data:updatedInfo } = await ExperiencesApi.updatePlace({
-     universeId: 5795192361, placeId: 16866553538, newData: { displayName: "Hello World" }
+     universeId: 5795192361, placeId: 16866553538, displayName: "Hello World"
    })
  * @exampleData {"path":"universes/5795192361/places/16866553538","createTime":"2024-03-25T10:42:46.297Z","updateTime":"2024-05-13T10:21:20.247Z","displayName":"Hello World","description":"","serverSize":50}
  * @exampleRawBody {"path":"universes/5795192361/places/16866553538","createTime":"2024-03-25T10:42:46.297Z","updateTime":"2024-05-13T10:21:20.247157600Z","displayName":"Hello World","description":"","serverSize":50}
